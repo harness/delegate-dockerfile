1287753c24b09aeeeb89bb4d697931e645399cb2
bot2_harness <bot2@harness.io>
2024-01-30 16:15:03 +0000

    Syncing docker file for 82108

--- immutable-scripts/start.sh
+++ immutable-scripts/start.sh
@@ -62,7 +62,7 @@ echo "heartbeatIntervalMs: 50000" >> config.yml
 echo "localDiskPath: /tmp" >> config.yml
 echo "maxCachedArtifacts: 2" >> config.yml
 echo "pollForTasks: ${POLL_FOR_TASKS:-false}" >> config.yml
-echo "grpcServiceEnabled: ${GRPC_SERVICE_ENABLED:-true}" >> config.yml
+echo "grpcServiceEnabled: ${GRPC_SERVICE_ENABLED:-false}" >> config.yml
 echo "grpcServiceConnectorPort: ${GRPC_SERVICE_CONNECTOR_PORT:-8080}" >> config.yml
 echo "doUpgrade: false" >> config.yml
 
